959824ffd52b854440542d710a28e181db55bf09
Oscar Valdés <ojvc22@gmail.com>
2024-04-15 13:03:15 -0600

    Initial commit, working project

---
+++ backend/flask_server.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+
+app = Flask(__name__)
+CORS(app)
+
+(_, _), (x_test, y_test) = mnist.load_data()
+x_test = x_test.astype('float32') / 255.0
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+model = tf.keras.models.load_model('mnist_cnn_model.h5')
+
+@app.route('/predict', methods=['POST'])
+
+# Function predict receives a stringified JSON object with the key 'image'
+# The value of 'image' is a (28, 28, 1) numpy array with pixel values between 0 and 1
+def predict():
+    data = request.get_json(force=True)
+    # print shape of data
+    print("Image shape: ", np.array(data['image']).shape)
+    # Convert canvas data to a numpy array
+    image = np.array(data['image'], dtype=np.float32)
+    reshaped_image = image.reshape(1,image.shape[0],image.shape[1],image.shape[2])
+    prediction = model.predict([reshaped_image])
+    predicted_class = int(np.argmax(prediction))
+    return jsonify(prediction=predicted_class)
+
+@app.route('/random', methods=['GET'])
+def random_image():
+    random_index = np.random.randint(len(x_test))
+    image = x_test[random_index].tolist()  # Convert numpy array to list
+    label = int(y_test[random_index])
+    return jsonify(image=image, label=label)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ frontend/app/src/App.js
@@ -0,0 +1,162 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [pixels, setPixels] = useState(Array(28 * 28).fill(0));
+  const drawing = useRef(false);
+  const [darkMode, setDarkMode] = useState(false);
+  const [prediction, setPrediction] = useState('');
+  const [label, setLabel] = useState('');
+  const [brushSize, setBrushSize] = useState(2);
+
+  const handleClear = () => {
+    setPixels(Array(28 * 28).fill(0));
+    setPrediction('');
+    setLabel('');
+  }
+
+  const distance = (x1, y1, x2, y2) => {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  };
+
+  const MAX_BRUSH_OPACITY = 255; // Maximum intensity value for painting
+
+  const getBrushedPixels = (pixels, index, brushSize, maxBrushOpacity) => {
+    const newPixels = [...pixels];
+    const x = index % 28;
+    const y = Math.floor(index / 28);
+
+    for (let i = -brushSize; i <= brushSize; i++) {
+      for (let j = -brushSize; j <= brushSize; j++) {
+        const distanceFromCenter = distance(0, 0, i, j);
+        if (distanceFromCenter > brushSize) continue;
+
+        const brushIntensity = maxBrushOpacity * (1 - (distanceFromCenter / brushSize));
+        const xi = x + i;
+        const yi = y + j;
+        if (xi >= 0 && xi < 28 && yi >= 0 && yi < 28) {
+          const pixelIndex = yi * 28 + xi;
+          newPixels[pixelIndex] = Math.min(
+            maxBrushOpacity,
+            newPixels[pixelIndex] + brushIntensity
+          );
+        }
+      }
+    }
+
+    return newPixels;
+  };
+
+  const handleMouseOver = (index) => {
+    if (drawing.current) {
+      const newPixels = getBrushedPixels(pixels, index, brushSize, MAX_BRUSH_OPACITY);
+      setPixels(newPixels);
+    }
+  };
+
+  const handleMouseDown = (index) => {
+    drawing.current = true;
+    const newPixels = getBrushedPixels(pixels, index, brushSize, MAX_BRUSH_OPACITY);
+    setPixels(newPixels);
+  };
+
+  const handleMouseUp = () => {
+    drawing.current = false;
+  };
+
+  const handleRandom = () => {
+    fetch('http://127.0.0.1:5000/random')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const newPixels = data.image.flat(); // Flatten the 2D array to a 1D array directly
+        setPixels(newPixels.map(value => value * 255)); // Ensure proper scaling if needed
+        setLabel(data.label);
+        setPrediction('');
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+  };
+
+  const handleGuess = () => {
+    // Image data must have values from [0, 1] and be in the shape (28, 28, 1)
+    const normalizedPixels = pixels.map(value => value / 255);
+    const imageData = {
+    image: Array.from({ length: 28 }, (_, i) =>
+      normalizedPixels.slice(i * 28, i * 28 + 28).map(value => [value])
+      ),
+    };
+
+    fetch('http://127.0.0.1:5000/predict', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(imageData)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setPrediction(data.prediction);
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+        setPrediction('Error in prediction');
+      });
+  };
+
+  return (
+    <div className={`app-container ${darkMode ? 'dark-mode' : ''}`}>
+      <h1>Handwritten Digit Recognition</h1>
+      <div className="grid-container" onMouseUp={handleMouseUp}>
+        <div className="grid">
+          {pixels.map((value, index) => (
+            <div
+              key={index}
+              onMouseDown={() => handleMouseDown(index)}
+              onMouseOver={() => handleMouseOver(index)}
+              style={{ backgroundColor: `rgb(${darkMode?value: 255-value}, ${darkMode?value: 255-value}, ${darkMode?value: 255-value})` }}
+              className="pixel"
+            />
+          ))}
+        </div>
+        <p className="true-label">True Label: {label !== '' ? label : "unknown"}</p>
+      </div>
+      <div className="buttons">
+        <button onClick={handleClear}>Clear</button>
+        <button onClick={handleRandom}>Random</button>
+        <button onClick={handleGuess}>Guess</button>
+      </div>
+      <p className="prediction">Model prediction: {prediction}</p>
+      <div className="brush-size">
+        <label>Brush size: {brushSize}</label>
+        <input
+          type="range"
+          min="1"
+          max="3"
+          value={brushSize}
+          onChange={(e) => setBrushSize(parseInt(e.target.value))}
+        />
+      </div>
+      <div className="dark-mode">
+        <label>Dark mode</label>
+        <input
+          type="checkbox"
+          checked={darkMode}
+          onChange={() => setDarkMode(!darkMode)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
